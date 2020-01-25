1bdda6dbd42c28f5874affaa980c883cf2311d7f
Gleb Martianov <margleb@yandex.ru>
2020-01-25 13:11:58 +0300

    Getting User Input

--- src/Application.java
+++ src/Application.java
@@ -1,24 +1,19 @@
+import java.util.Scanner;
+
 public class Application {
-	public static void main(String[] args) {
-
-		int myInt = 15;
-		if(myInt < 10) {
-			System.out.println("Yes, it's true!");
-		} else if(myInt > 20) {
-			System.out.println("Yes, it's false!");
-		} else {
-			System.out.println("Non of the above.");
-		}
-
-		int loop = 0;
-		while (loop < 5) {
-			System.out.println("Looping:" + loop);
-			if (loop == 5) {
-				break;
-			}
-			loop++;
-			System.out.println("Running");
-		}
-
+	public static void main(String[] args) {	
+		// Создаем обьект scanner
+		Scanner input = new Scanner(System.in);
+		
+		// Выводим сообщение в консоль
+		System.out.println("Eneter a floating point value: ");
+		
+		// Ожидаем ввод пользователем текста
+		// String line = input.nextLine();
+		// int value = input.nextInt();
+		double value = input.nextDouble();
+		
+		// Выводим введенное сообщение
+		System.out.println("You entered: " + value);
 	}
 }
