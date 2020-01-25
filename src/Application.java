dd99efccc59b91ae5b5f9b2d5be13e79ac75f235
Gleb Martianov <margleb@yandex.ru>
2020-01-25 12:42:26 +0300

    For Loops

--- src/Application.java
+++ src/Application.java
@@ -1,15 +1,7 @@
-
 public class Application {
 	public static void main(String[] args) {
-		// int value = 10;
-		// boolean loop = value < 20;
-		// System.out.println(loop);
-		
-		int value = 0;
-		while(value < 5) {
-			System.out.println("Hello " + value);
-			value = value + 1;
+		for(int i = 0; i < 5; i++) {
+			System.out.printf("The value of i is %d\n", i);
 		}
-		
 	}
 }
