12285d803f957d4c8696ffd62df37217a85ed5b8
Gleb Martianov <margleb@yandex.ru>
2020-01-25 14:07:53 +0300

    Arrays of Strings

--- src/Application.java
+++ src/Application.java
@@ -1,35 +1,30 @@
-import java.util.Scanner;
-
 public class Application {
 	public static void main(String[] args) {
 		
-		int value = 7;
+	    // создание массива words и резерв. памяти
+		String[] words = new String[3];
+		// присваивание
+		words[0] = "Hello";
+		words[1] = "to";
+		words[2] = "you";
 		
-		// массив
-		int[] values;
-		// резервируем памят для 3ех int
-		values = new int[3];
+		System.out.println(words[2]);
 		
-		System.out.println(values[0]);
+		// создание массива и присваивание
+		String[] fruits = {"apple", "banana", "pear", "kiwi"};
 		
-		values[0] = 10;
-		values[1] = 20;
-		values[2] = 30;
-		
-		// System.out.println(values[0]);
-		// System.out.println(values[1]);
-		// System.out.println(values[2]);
-		
-		for(int i=0; i<values.length; i++) {
-			System.out.println(values[i]);
+		for(String fruit: fruits) {
+			System.out.println(fruit);
 		}
 		
-		// способ присваивания без резерв. памяти
-		int[] numbers = {5,6,7};
+		// int value = 0;
+		// string в отличие от примитивных типов данных не выделяет память
+		// а лишь сосдат ссылку с пустым значением null
+		// String text = null;
 		
-		for(int i=0; i<numbers.length; i++) {
-			System.out.println(numbers[i]);
-		}
+		String[] texts = new String[2];
+		System.out.println(texts[0]); // null
+		texts[0] = "one"; // выделение памяти
 		
 	}
 }
