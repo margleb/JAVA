0242154b8609c1a8ebc177fd5c713637163dea02
Gleb Martianov <margleb@yandex.ru>
2020-01-25 16:16:45 +0300

    Methods

--- src/Application.java
+++ src/Application.java
@@ -6,21 +6,34 @@ class Person {
 	// Classes can contain
 	// 1. Data
 	// 2. Subroutines (methods)
+	void speak() {
+		for(int i=0; i<3; i++) {
+			System.out.println("My name is: " + name + " and I am " + age + " years old ");	
+		}
+	}
+	
+	void sayHello() {
+		System.out.println("Hello there!");
+	}
+	
 }
  
 
 public class Application {
 	public static void main(String[] args) {
-		
+		// Create a Person object using the Person class
 		Person person1 = new Person();
 		person1.name = "Joe Bloggs";
 		person1.age = 37;
+		person1.speak();
+		person1.sayHello();
 		
+		// Create a second Person object
 		Person person2 = new Person();
 		person2.name = "Sarah Smith";
 		person2.age = 20;
+		person2.speak();
 		
-		System.out.println(person1.name);
-		
+		System.out.println(person1.name);	
 	};
 }
