e0e488c3ddcd4ad8f1c9e4999951949baa649958
Gleb Martianov <margleb@yandex.ru>
2020-01-25 12:57:32 +0300

    if/else

--- src/Application.java
+++ src/Application.java
@@ -1,7 +1,24 @@
 public class Application {
 	public static void main(String[] args) {
-		for(int i = 0; i < 5; i++) {
-			System.out.printf("The value of i is %d\n", i);
+
+		int myInt = 15;
+		if(myInt < 10) {
+			System.out.println("Yes, it's true!");
+		} else if(myInt > 20) {
+			System.out.println("Yes, it's false!");
+		} else {
+			System.out.println("Non of the above.");
 		}
+
+		int loop = 0;
+		while (loop < 5) {
+			System.out.println("Looping:" + loop);
+			if (loop == 5) {
+				break;
+			}
+			loop++;
+			System.out.println("Running");
+		}
+
 	}
 }
