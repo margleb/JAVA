9464d2216d5e794264fa815a52aa49f8cac4cec4
Gleb Martianov <margleb@yandex.ru>
2020-01-25 16:03:37 +0300

    Classes and Objects

--- src/Application.java
+++ src/Application.java
@@ -1,37 +1,26 @@
+class Person {
+	// Instance variables (data of "state")
+	String name;
+	int age;
+	
+	// Classes can contain
+	// 1. Data
+	// 2. Subroutines (methods)
+}
+ 
+
 public class Application {
 	public static void main(String[] args) {
-
-		int[] values = { 3, 5, 2343 };
-		// System.out.println(values[2]);
-
-		int[][] grid = { { 3, 5, 2343 }, { 2, 4 }, { 1, 2, 3, 4 } };
-
-		// System.out.println(grid[1][1]);
-		// System.out.println(grid[0][2]);
 		
-		String[][] texts = new String[2][3];
-		texts[0][1] = "Hello there!";
+		Person person1 = new Person();
+		person1.name = "Joe Bloggs";
+		person1.age = 37;
 		
-		// System.out.println(texts[0][1]);
+		Person person2 = new Person();
+		person2.name = "Sarah Smith";
+		person2.age = 20;
 		
-		for(int row=0; row<grid.length; row++) {
-			for(int col=0; col < grid[row].length; col++) {
-				// System.out.print(grid[row][col] + "\t");
-			}
-			// System.out.println();
-		}
-		
-		// первый этап резервирования памяти
-		String[][] words = new String[2][];
-		// System.out.println(words[0]);
-		
-		// второй этап резервирования памяти
-		words[0] = new String[3];
-		
-		words[0][1] = "hi there";
-		
-		System.out.println(words[0][1]);
+		System.out.println(person1.name);
 		
 	};
-
 }
