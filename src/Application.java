5b342360054311cf3091f721dc102d6fcdc075a7
Gleb Martianov <margleb@yandex.ru>
2020-01-25 11:33:15 +0300

    Strings: Working With Text

--- src/Application.java
+++ src/Application.java
@@ -1,33 +1,21 @@
 
 public class Application {
-
 	public static void main(String[] args) {
-		// true/false
-		boolean isActive = false;
-		// целое число, занимает 1 байт
-		byte myByte = 127;
-		// целое число, занимает 2 байта
-		short myShort = -32768;
-		// целое число, занимает 4 байта
-		int myNumber = 2147483647;
-		// целое число, занимает 8 байт
-		long myLong = 5;
-		// число с плавающей точкой, занимает 8 байт
-		double myDouble = 8.5;
-		// число с плавающей точкой занимает 4 байта
-		float myFloat = 8.5F;
-		// хранит одиночный символ в кодировке UTF-16 и занимает 2 байта
-		char myChar = 'g';
 		
-		System.out.println(isActive);
-		System.out.println(myByte);
-		System.out.println(myShort);
-		System.out.println(myNumber);
-		System.out.println(myLong);
-		System.out.println(myDouble);
-		System.out.println(myFloat);
-		System.out.println(myChar);
+		// класс string (не примитивный тип данных)
+		String text = "Hello";
+		String blank = " ";
+		String name = "Bob";
+		String greeting = text + blank + name;
 		
+		System.out.println(greeting);
+		System.out.println("Hello" + " " + "Jane");
+		
+		int myInt = 7;
+		System.out.println("My integer is: " + myInt);
+		
+		double myDouble = 7.8;
+	    System.out.println("My double is: " + myDouble + ".");
+	
 	}
-
 }
