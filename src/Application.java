ebb2156e92126bc5c32753479a330d931953c133
Gleb Martianov <margleb@yandex.ru>
2020-01-27 18:16:45 +0300

    Creating and Writing Text Files

--- src/Application.java
+++ src/Application.java
@@ -1,21 +1,22 @@
-class Temp implements AutoCloseable {
-
-	@Override
-	public void close() throws Exception {
-		System.out.println("Closing!");
-		// throw new Exception("oh no!");
-	}
-	
-}
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Application {
 	public static void main(String[] args) {
-		try(Temp temp = new Temp()) {
-			
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		File file = new File("test.txt");
+		
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
+			bw.write("This is line one");
+			bw.newLine();
+			bw.write("This is line two");
+			bw.newLine();
+			bw.write("Last line.");
+		} catch(IOException e) {
+			System.out.println("Unable to read file " + file.toString());
 		}
+		
 		
 	}
 }
