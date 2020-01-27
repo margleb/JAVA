a361df4869061db4b3958cc2addc965ad402d8df
Gleb Martianov <margleb@yandex.ru>
2020-01-27 11:57:25 +0300

    Generics and Wildcards

--- src/Application.java
+++ src/Application.java
@@ -1,46 +1,72 @@
 import java.util.ArrayList;
 
+class Machine {
 
-class Animal {
+	public String toString() {
+		return "I am a machine";
+	}
+
+	public void start() {
+		System.out.println("Machine starting.");
+	}
+}
+
+class Camera extends Machine {
+
+	public String toString() {
+		return "I am a сamera";
+	}
+	
+	public void snap() {
+		System.out.println("snap!");
+	}
 	
 }
 
 public class Application {
-	
+
 	public static void main(String[] args) {
+
+		ArrayList<Machine> list1 = new ArrayList<Machine>();
+
+		list1.add(new Machine());
+		list1.add(new Machine());
+
+		ArrayList<Camera> list2 = new ArrayList<Camera>();
+
+		list2.add(new Camera());
+		list2.add(new Camera());
 		
-		///////////////// Before Java 5 ///////////////
-		ArrayList list = new ArrayList();
-		
-		list.add("apple");
-		list.add("banana");
-		list.add("orange");
-		
-		// get возращает обьект, поэтому его необходимо downcasting в cтроку
-		String fruit = (String) list.get(1);
-		
-		System.out.println(fruit);
-		
-		///////////// Modern style //////////////////
-		
-		// Указываем в квадратных скобках тип который сразу же хотим
-		ArrayList<String> strings = new ArrayList<String>();
-		
-		strings.add("сat");
-		strings.add("dog");
-		strings.add("alligator");
-		
-		String animal = strings.get(1);
-		System.out.println(animal);
-		
-		//////////// There can be more than one type argument //////////////
-		// HashMap<Integer, String> map = new HashMap<Integer,String>();
-		
+		showList2(list2);
+		showList(list1);
+		showList3(list1);
+	}
+
+	// вопросительный знак означает прочитать с неизвестным типом
+	// Object - cуперкласс позволяющий работыть с любим типом обьектов
+	// Extends чтобы мы не вставили в метод, это бует частю класса Machine
 	
-		////////// Java 7 style //////////////////////
-		
-		ArrayList<Animal> someList = new ArrayList<>();
-	
+	// Может быть класс Machine или сабкласс класса Machine
+	public static void showList(ArrayList<? extends Machine> list) {
+		// for (Object value : list) {
+		for (Machine value : list) {
+			System.out.println(value);
+			value.start();
+		}	
 	}
 	
+	// Может быть класс Camera или супер класс Camera
+	public static void showList2(ArrayList<? super Camera> list) {
+		for (Object value : list) {
+			System.out.println(value);
+		}	
+	}
+	
+	// Может быть любой класс
+	public static void showList3(ArrayList<?> list) {
+			for (Object value : list) {
+				System.out.println(value);
+			}	
+	}
+
 }
