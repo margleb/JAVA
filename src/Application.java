5decbdfcb7102295ed495e6f04cf1e1e1826760e
Gleb Martianov <margleb@yandex.ru>
2020-01-27 13:23:01 +0300

    Anonymous Classes

--- src/Application.java
+++ src/Application.java
@@ -1,72 +1,31 @@
-import java.util.ArrayList;
-
 class Machine {
-
-	public String toString() {
-		return "I am a machine";
-	}
-
 	public void start() {
-		System.out.println("Machine starting.");
+		System.out.println("Starting machine...");
 	}
 }
 
-class Camera extends Machine {
-
-	public String toString() {
-		return "I am a сamera";
-	}
-	
-	public void snap() {
-		System.out.println("snap!");
-	}
-	
+interface Plant {
+	public void grow();
 }
 
 public class Application {
 
 	public static void main(String[] args) {
-
-		ArrayList<Machine> list1 = new ArrayList<Machine>();
-
-		list1.add(new Machine());
-		list1.add(new Machine());
-
-		ArrayList<Camera> list2 = new ArrayList<Camera>();
-
-		list2.add(new Camera());
-		list2.add(new Camera());
+		// переписанный класс является потом родительского
+		Machine machine1 = new Machine() {
+			@Override public void start() {
+				System.out.println("Camera snapping...");
+			}
+		};
+		machine1.start();
 		
-		showList2(list2);
-		showList(list1);
-		showList3(list1);
+		// внедрение метода через интерфейс
+		Plant plant1 = new Plant() {
+			public void grow() {
+				System.out.println("Plant growing");	
+			}
+		};
+		
+		plant1.grow();
 	}
-
-	// вопросительный знак означает прочитать с неизвестным типом
-	// Object - cуперкласс позволяющий работыть с любим типом обьектов
-	// Extends чтобы мы не вставили в метод, это бует частю класса Machine
-	
-	// Может быть класс Machine или сабкласс класса Machine
-	public static void showList(ArrayList<? extends Machine> list) {
-		// for (Object value : list) {
-		for (Machine value : list) {
-			System.out.println(value);
-			value.start();
-		}	
-	}
-	
-	// Может быть класс Camera или супер класс Camera
-	public static void showList2(ArrayList<? super Camera> list) {
-		for (Object value : list) {
-			System.out.println(value);
-		}	
-	}
-	
-	// Может быть любой класс
-	public static void showList3(ArrayList<?> list) {
-			for (Object value : list) {
-				System.out.println(value);
-			}	
-	}
-
 }
