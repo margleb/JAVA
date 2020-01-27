fdd6c674ca73fc821bc16ca2d72d0c79967d4df5
Gleb Martianov <margleb@yandex.ru>
2020-01-27 18:02:11 +0300

    Try-With-Resources

--- src/Application.java
+++ src/Application.java
@@ -1,40 +1,21 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+class Temp implements AutoCloseable {
+
+	@Override
+	public void close() throws Exception {
+		System.out.println("Closing!");
+		// throw new Exception("oh no!");
+	}
+	
+}
 
 public class Application {
-
 	public static void main(String[] args) {
-		
-		File file = new File("test.txt1");
-		BufferedReader br = null;
-
-		try {
-			FileReader fr = new FileReader(file);
-			br = new BufferedReader(fr);
-			String line;
-			while ((line = br.readLine()) != null) {
-				System.out.println(line);
-			}
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found: " + file.toString());
-		} catch (IOException e) {
+		try(Temp temp = new Temp()) {
+			
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
-			System.out.println("Unable to read file: " + file.toString());
+			e.printStackTrace();
 		}
-		// finally вызывается всегда вне зависимости от появившейся ошибки
-		finally {
-			try {
-				br.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				System.out.println("Unable to close file: " + file.toString());
-			} catch(NullPointerException ex) {
-				// File was probably never opened!
-				System.out.println("");
-			}	
-		}
+		
 	}
 }
