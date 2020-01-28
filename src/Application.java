d81cd0cef0818b8ddba9b0d1db2d1504aa2db574
Gleb Martianov <margleb@yandex.ru>
2020-01-28 16:35:43 +0300

    ArrayList: Arrays the Easy Way

--- src/Application.java
+++ src/Application.java
@@ -1,8 +1,38 @@
+import java.util.ArrayList;
 
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		
+		// Adding
+		numbers.add(10);
+		numbers.add(100);
+		numbers.add(40);
+		
+		// Retrieving
+		// System.out.println(numbers.get(0));
+		
+		System.out.println("\nIteration #1: ");
+		// Indexed for loop iteration
+		for(int i=0; i < numbers.size(); i++) {
+			System.out.println(numbers.get(i));
+		}
+		
+		// Removing items (careful!)
+		numbers.remove(numbers.size() - 1);
+		
+		// This is VERY slow
+		numbers.remove(0);
+		
+		System.out.println("\nIteration #2: ");
+		for(Integer value: numbers) {
+			System.out.println(value);
+		}
+		
+		// List interface ...
+		List<String> values = new ArrayList<String>();
+		
 	}
 
 }
