f085a71ccad8ca6f76ee8c982bcad1ee30c7c71f
Gleb Martianov <margleb@yandex.ru>
2020-01-28 14:17:33 +0300

    Serializing Arrays

--- src/ReadObjects.java
+++ src/ReadObjects.java
@@ -2,24 +2,43 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.ArrayList;
 
 public class ReadObjects {
 	public static void main(String[] args) {
 		System.out.println("Reading objects...");
 		
-		try(FileInputStream fi = new FileInputStream("people.bin")) {
+		try(FileInputStream fi = new FileInputStream("people.bin"); ObjectInputStream os = new ObjectInputStream(fi)) {
 			
-			ObjectInputStream os = new ObjectInputStream(fi);
+			// прочитываем массив обьектов из файла
+			Person[] people = (Person[]) os.readObject();
 			
-			// необходим кастинг так как возращает осн. обьект
-			Person person1 = (Person) os.readObject();
-			Person person2 = (Person) os.readObject();
+			// прочитываем массив из файла
+			// @SuppressWarnings - убирает предупреждения
+			// передаем в casting Person, так как при десериализации теряется тип
+			@SuppressWarnings("unchecked")
+			ArrayList<Person> peopleList = (ArrayList<Person>)os.readObject();
 			
-			os.close();
+			for(Person person: people) {
+				System.out.println(person);
+			}
 			
-			System.out.println(person1);
-			System.out.println(person2);
-						
+			// выводим значения массива из файла
+			for(Person person: peopleList) {
+				System.out.println(person);
+			}
+			
+			// прочитываем кол-во обьектов в массиве
+			int num = os.readInt();
+			
+			for(int i=0; i<num; i++) {
+				// прочитываем каждый обьект
+				Person person = (Person) os.readObject();
+				System.out.println(person);
+			}
+			
+			// os.close();
+		
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- src/WriteObjects.java
+++ src/WriteObjects.java
@@ -2,6 +2,8 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class WriteObjects {
 	public static void main(String[] args) {
@@ -13,13 +15,28 @@ public class WriteObjects {
 		System.out.println(mike);
 		System.out.println(sue);
 		
+		Person[] people = {new Person(1, "Sue"), new Person(99, "Mike"), new Person(7, "Bob")};
+		
+		// преобазуем обьект в массив
+		ArrayList<Person> peopleList = new ArrayList<Person>(Arrays.asList(people));
+		
 		try(FileOutputStream fs = new FileOutputStream("people.bin")) {
 			
 			ObjectOutputStream os = new ObjectOutputStream(fs);
 			
-			// сериализируем обьекты
-			os.writeObject(mike);
-			os.writeObject(sue);
+			// cериализуем обьект
+			os.writeObject(people);
+			
+			// сериалиуем массив
+			os.writeObject(peopleList);
+			
+			// сериализуем длину элементов обьекта
+			os.writeInt(peopleList.size());
+			
+			for(Person person: peopleList) {
+				os.writeObject(person);
+			}
+		
 			
 			os.close();
 			
