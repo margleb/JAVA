84b81059065e1ddb2fcc586f1d975b0aa40371bb
Gleb Martianov <margleb@yandex.ru>
2020-01-28 11:56:41 +0300

    Recursion: A Useful Trick Up Your Sleeve

--- src/Application.java
+++ src/Application.java
@@ -1,41 +1,17 @@
 public class Application {
 
-	// public static final int DOG = 0;
-	// public static final int CAT = 1;
-	// public static final int MOUSE = 2;
-
 	public static void main(String[] args) {
+		// E.g 4! = 4*3*2*1 (factorial 4)
+		System.out.println(factorial(4));
+		
+	}
 
-		Animal animal = Animal.DOG;
-
-		switch (animal) {
-		case CAT:
-			System.out.println("Cat");
-			break;
-		case DOG:
-			System.out.println("Dog");
-			break;
-		case MOUSE:
-			System.out.println("Mouse");
-			break;
-		default:
-			break;
+	private static int factorial(int value) {
+		System.out.println(value);
+		if (value == 1) {
+			return 1;
 		}
-		
-		// выводит как строку если у константы не передано имя
-		// выводит значение метода toString()
-		System.out.println(Animal.DOG);
-		// выводит название константы как строку
-		System.out.println("Enum name as a string " + Animal.DOG.name());
-		// выводит класс Animal
-		System.out.println(Animal.DOG.getClass());
-		// instanceof оператор проверят является ли экземпляром класса Animal
-		System.out.println(Animal.DOG instanceof Enum);
-		// получение имени константы MOUSE из Enum
-		System.out.println(Animal.MOUSE.getName());
-		// получаем значение константы CAT
-		Animal animal2 = Animal.valueOf("CAT");
-		System.out.println(animal2);
-		
+		// рекрусинвный вызов метода
+		return factorial(value - 1) * value;
 	}
 }
