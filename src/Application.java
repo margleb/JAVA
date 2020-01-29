75bba2cf2dcf71c8ea28fbe5fe94fa436388e978
Gleb Martianov <margleb@yandex.ru>
2020-01-29 10:55:03 +0300

    Sorted Maps

--- src/Application.java
+++ src/Application.java
@@ -1,32 +1,40 @@
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Application {
 	public static void main(String[] args) {
+		Map<Integer, String> hashMap = new HashMap<Integer, String>();
+		// LinkedHashMap позволяет держать ключ/значение в порядке добавляемом в Map
+		Map<Integer, String> linkedHashMap = new LinkedHashMap<Integer, String>();
+		// Держит элементы в натуральном порядке (1,2,3.., a,b,c...)
+		Map<Integer, String> treeMap = new TreeMap<Integer, String>();
 		
-		// HahsMap НЕ поддерживает порядок элементов (иногда выводится рандомно)
-		HashMap<Integer, String> map = new HashMap<Integer, String>();
+		// выводит произвольнои (иногда может менять порядок)
+		testMap(hashMap);
+		System.out.println();
+		// выводит в порядке добавления
+		testMap(linkedHashMap);
+		System.out.println();
+		// в натуральном порядке
+		testMap(treeMap);
+	}
+	
+	public static void testMap(Map<Integer, String> map) {
+		map.put(9, "fox");
+		map.put(4, "cat");
+		map.put(8, "dog");
+		map.put(1, "giraffe");
+		map.put(0, "swan");
+		map.put(15, "bear");
+		map.put(6, "snake");
 		
-		map.put(5, "Five");
-		map.put(8, "Eight");
-		map.put(6, "Six");
-		// перезапись дубля
-		// map.put(6, "Hello");
-		map.put(4, "Four");
-		map.put(2, "Two");
-		
-		String text = map.get(6);
-		
-		// System.out.println(text);
-		
-		for(Map.Entry<Integer, String> entry: map.entrySet()) {
-			// получить ключ
-			int key = entry.getKey();
-			// получить значение
-			String value = entry.getValue();
+		for(Integer key: map.keySet()) {
+			String value = map.get(key);
 			System.out.println(key + ": " + value);
-			
 		}
 		
-	}	
+	}
+	
 }
