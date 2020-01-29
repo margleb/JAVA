c57e88cf26acfb840d45f986f9e7ca95f411772e
Gleb Martianov <margleb@yandex.ru>
2020-01-29 11:35:57 +0300

    Sets

--- src/Application.java
+++ src/Application.java
@@ -1,40 +1,93 @@
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Application {
+
 	public static void main(String[] args) {
-		Map<Integer, String> hashMap = new HashMap<Integer, String>();
-		// LinkedHashMap позволяет держать ключ/значение в порядке добавляемом в Map
-		Map<Integer, String> linkedHashMap = new LinkedHashMap<Integer, String>();
-		// Держит элементы в натуральном порядке (1,2,3.., a,b,c...)
-		Map<Integer, String> treeMap = new TreeMap<Integer, String>();
-		
-		// выводит произвольнои (иногда может менять порядок)
-		testMap(hashMap);
-		System.out.println();
-		// выводит в порядке добавления
-		testMap(linkedHashMap);
-		System.out.println();
-		// в натуральном порядке
-		testMap(treeMap);
-	}
-	
-	public static void testMap(Map<Integer, String> map) {
-		map.put(9, "fox");
-		map.put(4, "cat");
-		map.put(8, "dog");
-		map.put(1, "giraffe");
-		map.put(0, "swan");
-		map.put(15, "bear");
-		map.put(6, "snake");
-		
-		for(Integer key: map.keySet()) {
-			String value = map.get(key);
-			System.out.println(key + ": " + value);
+
+		// Hashset не сохраняет порядок
+		// Set<String> set1 = new HashSet<String>();
+
+		// LinkedHashSet сохраняет порядок добавления элементов
+		// Set<String> set1 = new LinkedHashSet<String>();
+
+		// TreeSet устанавливает элементы в натуральном порядке (1,2,3.., a,b,c...)
+		Set<String> set1 = new TreeSet<String>();
+
+		if (set1.isEmpty()) {
+			System.out.println("Set is empty at start");
 		}
+
+		System.out.println();
+
+		set1.add("dog");
+		set1.add("cat");
+		set1.add("mouse");
+		set1.add("snake");
+		set1.add("bear");
+
+		if (set1.isEmpty()) {
+			System.out.println("Set is empty after adding");
+		}
+
+		// Добаление дубликатов не учитывается
+		set1.add("mouse");
+
+		System.out.println(set1);
+
+		///////////// Iteration ///////////////////
+		System.out.println();
+
+		for (String element : set1) {
+			System.out.println(element);
+		}
+
+		///////////// Does set contains a given item? ///////////////////
+		System.out.println();
+
+		if (set1.contains("aadvark")) {
+			System.out.println("Contains aadvark");
+		}
+
+		if (set1.contains("cat")) {
+			System.out.println("Contains cat");
+		}
+
+		// set2 contains some common elements with set1, and some new
+		Set<String> set2 = new TreeSet<String>();
 		
-	}
+		set2.add("dog");
+		set2.add("cat");
+		set2.add("giraffe");
+		set2.add("monkey");
+		set2.add("ant");
+		
+		///////// Intersection ////////////////////////
+		System.out.println();
+		
+		Set<String> intersection = new HashSet<String>(set1);
+		
+		System.out.println(intersection);
 	
+		//  оставляет только элементы содержащиеся в set1 и set2
+		intersection.retainAll(set2);
+		
+		System.out.println();
+		
+		System.out.println(intersection);
+		
+		///////// Diffrence ////////////////////////
+		System.out.println();
+		
+		Set<String> difference = new HashSet<String>(set2);
+		
+		// удаляет элементы содержащиеся в set1 и set2
+		difference.removeAll(set1);
+		
+		System.out.println(difference);
+
+	}
+
 }
